8ceee2fc0b94d771e71d6cce72eb75a24a64488a
Dapucla <55713843+Dapucla@users.noreply.github.com>
2021-01-18 02:34:32 +0300

    Update models.py

--- mainapp/models.py
+++ mainapp/models.py
@@ -99,7 +99,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
     title = models.CharField(max_length=255, verbose_name='Наименование')
     slug = models.SlugField(unique=True)
-    image = models.ImageField(verbose_name='Изображение')
+    image = models.ImageField(upload_to='media/', verbose_name='Изображение')
     description = models.TextField(verbose_name='Описание', null=True)
     price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Цена')
 
