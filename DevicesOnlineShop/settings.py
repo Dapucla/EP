3b27f876fbf742a172867eb5949e690a2c952658
Daniil Alekseev <dapcula08@yandex.ru>
2021-07-11 12:10:22 +0300

    static

--- DevicesOnlineShop/settings.py
+++ DevicesOnlineShop/settings.py
@@ -140,20 +140,20 @@ USE_TZ = True
 IMPORT_EXPORT_USE_TRANSACTIONS = True
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'front/build/static'),
-    BASE_DIR / "static"
+   os.path.join(BASE_DIR, 'front/build/static'),
+   BASE_DIR / "static"
 ]
 
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static"
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
